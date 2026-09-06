99b714b1158cddf65e5fe391d4d84dba6eb3207c
licomnaklavy <andrey.vtorov.2@gmail.com>
2026-09-06 20:33:26 +0300

    fix

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const jsonServer = require('json-server');
+const express = require('express');
 const server = jsonServer.create();
 const router = jsonServer.router('mc-database.json');
 const middlewares = jsonServer.defaults();
 
+server.use(express.static('public'));
 server.use(middlewares);
 server.use(router);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-  console.log(`Items: https://apipracticwork-production-1ab5.up.railway.app/items`);
-  console.log(`Mobs: https://apipracticwork-production-1ab5.up.railway.app/mobs`);
-  console.log(`Blocks: https://apipracticwork-production-1ab5.up.railway.app/blocks`);
+  console.log(`Сервер запущен на порту ${PORT}`);
 });
